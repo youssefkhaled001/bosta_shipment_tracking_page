5f1c7dc1c5ff5d2af7d5d90c58fcc7de378fcf3a
Youssef Khaled <youssefkhaled001@outlook.com>
2025-01-18 18:46:37 +0200

    Added Page texts Constants

---
+++ src/constants.ts
@@ -0,0 +1,71 @@
+export const HeaderTexts = {
+        HeaderText: {
+            "en": 'Track Your Order',
+            "ar": 'تتبع طلبك'
+        },
+        SearchPlaceholder: {
+            "en": 'Enter Tracking Number',
+            "ar": 'أدخل الرقم التتبعي'
+        }
+}
+export const TimeLineTexts = {
+    ShipmentCreated: {
+        "en": 'Shipment Created',
+        "ar": 'تم إنشاء الطلبية'
+    },
+    PickedUp: {
+        "en": 'Picked Up',
+        "ar": 'تم الاستلام'
+    },
+    InTransit: {
+        "en": 'In Transit',
+        "ar": 'في النقل'
+    },
+    OutForDelivery: {
+        "en": 'Out for Delivery',
+        "ar": 'قيد التوصيل'
+    },
+    Delivered: {
+        "en": 'Delivered',
+        "ar": 'تم التوصيل'
+    },
+}
+export const TrackingDetailsText = {
+    "en": 'Tracking Details',
+    "ar": 'تتبع التفاصيل'
+}
+export const StatusTexts = {
+    Loading:{
+        "en": 'Loading',
+        "ar": 'جاري التحميل'
+    },
+    SomethingWentWrong: {
+        "en": 'Something went wrong, please try again later',
+        "ar": 'حدث خطأ ما, يرجى المحاولة مرة أخرى في وقت لاحق'
+    },
+    NoShipmentFound: {
+        "en": 'No Shipment Found with this Tracking Number',
+        "ar": 'لم يتم العثور على طلبية بهذا الرقم التتبعي'
+    },
+    WaitingInput: {
+        "en": "Please Enter a Tracking Number",
+        "ar": "يرجى إدخال الرقم التتبعي"
+    },
+    NoEvents: {
+        "en": "No Events Found",
+        "ar": "لم يتم العثور على أي حدث"
+    },
+    Delivered: {
+        "en": "Delivered At",
+        "ar": "تم التوصيل يوم"
+    },
+    Returned: {
+        "en": "Returned At",
+        "ar": "تم الاسترجاع يوم"
+    },
+    Arriving: {
+        "en": "Arriving by",
+        "ar": "سوف يصل قبل يوم"
+    }
+}
+export const Bosta_API_Route = 'https://tracking.bosta.co/shipments/track/'
